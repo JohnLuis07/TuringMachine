16c0078edfca21c35cc301b3009c647566623fd0
John Luis Magtoto <jfmagtoto@up.edu.ph>
2024-01-18 17:38:31 +0800

    add attributes

--- ProjectMultiTapeTM.py
+++ ProjectMultiTapeTM.py
@@ -1,7 +1,33 @@
 class State:
-    def __init__(self):
+    def __init__(self, name, final=False):
+        self.name = name
+        self.final = final
+        self.next = {}
+
+    def delta(self, symbol):
         pass
+
+    def set_transition(self, input_symbol, write_symbol, next_state, direction):
+        pass
+
 
 class TuringMachine:
     def __init__(self):
         pass
+
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
+    def read_symbol(self):
+        pass
+
+    def write_symbol(self, symbol):
+        pass
+
+    def transition(self):
+        pass
+
+    
