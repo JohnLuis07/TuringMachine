7c221975f86ba6d65842f8dc6e8a3a68321e460b
John Luis Magtoto <jfmagtoto@up.edu.ph>
2024-01-19 17:27:20 +0800

    Update the attributes under class state and input sample transition function. Also updated the README.

--- ProjectMultiTapeTM.py
+++ ProjectMultiTapeTM.py
@@ -4,14 +4,27 @@ class State:
         self.final = final
         self.next = {}
 
+    #get the symbol from the tape list and the next state
     def delta(self, symbol):
-        pass
-
+        return self.next.get(symbol, []) + self.next.get('', [])
+    
+    #transition function that returns the next state, write symbol, and direction based on the input symbol.
     def set_transition(self, input_symbol, write_symbol, next_state, direction):
-        pass
+        if input_symbol not in self.next:
+            self.next[input_symbol] = []
+        #function that sets transitions
+        self.next[input_symbol].append({
+            'next_state': next_state,
+            'write_symbol': write_symbol,
+            'direction': direction
+        })
+
+    #set the state as final
+    def set_final(self):
+        self.final = True
 
 
-class TuringMachine:
+class MultiTapeTuringMachine:
     def __init__(self):
         pass
 
@@ -30,4 +43,28 @@ class TuringMachine:
     def transition(self):
         pass
 
-    
+#option A
+if __name__ == '__main__':
+    #define states
+
+    q0 = State('q0')
+    q1 = State('q1')
+    q2 = State('q2', final=True)
+
+    #L = left, R = right, S = stay
+    #define transitions self, input_symbol, write_symbol, next_state, direction
+    q0.set_transition(['0', '0', 'B'], ['0', '0', '0'], q0, ['L', 'L', 'L'])
+    q0.set_transition(['B', 'B', 'B'], ['B', 'B', '0'], q2, ['S', 'S', 'S'])
+    q0.set_transition(['0', '1', 'B'], ['0', '1', '1'], q0, ['L', 'L', 'L'])
+    q0.set_transition(['B', '1', 'B'], ['B', '1', '1'], q0, ['L', 'L', 'L'])
+    q0.set_transition(['1', '0', 'B'], ['1', '0', '1'], q0, ['L', 'L', 'L'])
+    q0.set_transition(['1', 'B', 'B'], ['1', 'B', '1'], q0, ['L', 'L', 'L'])
+    q0.set_transition(['1', '1', 'B'], ['1', '1', '0'], q1, ['L', 'L', 'L'])
+
+    q1.set_transition(['0', '0', 'B'], ['0', '0', '1'], q0, ['L', 'L', 'L'])
+    q1.set_transition(['B', 'B', 'B'], ['B', 'B', '1'], q2, ['S', 'S', 'S'])
+    q1.set_transition(['0', '1', 'B'], ['0', '1', '0'], q1, ['L', 'L', 'L'])
+    q1.set_transition(['B', '1', 'B'], ['B', '1', '0'], q1, ['L', 'L', 'L'])
+    q1.set_transition(['1', '0', 'B'], ['1', '0', '0'], q1, ['L', 'L', 'L'])
+    q1.set_transition(['1', 'B', 'B'], ['1', 'B', '0'], q1, ['L', 'L', 'L'])
+    q1.set_transition(['1', '1', 'B'], ['1', '1', '1'], q1, ['L', 'L', 'L'])
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TuringMachine
+
+NOTE: JUST CHANGE THE TRANSITION FUNCTION according to what do you want to happen to each of the turing machine in the main driver.
+
+Code content:
+1. Deterministic Turing Machine - produce one output.
+-DEFAULT TRANSITION FUNCTION: 2'S COMPLEMENT
+
+2. Nondeterministic Turing Machine - produce multiple outputs based on the transition function.
+-DEFAULT TRANSITION FUNCTION: 2'S COMPLEMENT WITH MULTIPLE OUTPUTS
+
+3. Deterministic Multitape Turing Machine - produce multiple tapes and output. 
+
+- 3 tapes
+	- first tape has the first input (in binary), tape head points to the rightmost input symbol
+	- second tape has the second input (in binary), tape head points to the rightmost input symbol
+	- third tape contains all "B", tape head points somewhere at the right
+	- all three tape heads are pointing on the same column
