88841bc9f35ed3b87428bfe8debbd06342db04bd
John Luis Magtoto <jfmagtoto@up.edu.ph>
2024-01-26 12:58:07 +0800

    Working

--- ProjectMultiTapeTM.py
+++ ProjectMultiTapeTM.py
@@ -34,70 +34,141 @@ class State:
 
 
 class MultiTapeTuringMachine:
-    def __init__(self, states, symbols, input_string, blank_symbol='B'):
+    def __init__(self, states, symbols, input_string1, input_string2, blank_symbol='B'):
         self.q = states
         self.sigma = symbols #alphabet
         self.blank_symbol = blank_symbol
-        self.tape = [blank_symbol] + list(input_string) + [blank_symbol] 
-        self.head_position = 1  # Start reading from the first character of the input
+        # tapes with the input strings at the rightmost end
+        self.tape1 = [blank_symbol] * (len(input_string1) + 1) + list(input_string1) + [blank_symbol]
+        self.tape2 = [blank_symbol] * (len(input_string2) + 1) + list(input_string2) + [blank_symbol] 
+        self.tape3 = [blank_symbol] * (len(input_string1) + 1) + [blank_symbol] * (len(input_string2) + 1) + [blank_symbol]
+        # Set initial head positions
+        self.head_position1 = len(self.tape1) - 2
+        self.head_position2 = len(self.tape2) - 2
+        self.head_position3 = len(self.tape3) - 2
         self.current_state = states[0]
 
-    def move_left(self):
-        for i in range(len(self.tapes)):
-            if self.head_positions[i] > 0:
-                self.head_positions[i] -= 1 #move left
+    def move_left(self, tape):
+        if tape == 1:
+            if self.head_position1 > 0:
+                self.head_position1 -= 1 # move left
             else:
-                self.tapes[i].insert(0, self.blank_symbol)
+                self.tape1.insert(0, self.blank_symbol)
+        elif tape == 2:
+            if self.head_position2 > 0:
+                self.head_position2 -= 1 # move left
+            else:
+                self.tape2.insert(0, self.blank_symbol)
+        elif tape == 3:
+            if self.head_position3 > 0:
+                self.head_position3 -= 1 # move left
+            else:
+                self.tape3.insert(0, self.blank_symbol)
 
-    def move_right(self):
-        for i in range(len(self.tapes)):
-            self.head_positions[i] += 1 #move right
-            if self.head_positions[i] == len(self.tapes[i]):
-                self.tapes[i].append(self.blank_symbol)
+    def move_right(self, tape):
+        if tape == 1:
+            self.head_position1 += 1 # move right
+            if self.head_position1 == len(self.tape1):
+                self.tape1.append(self.blank_symbol)
+        elif tape == 2:
+            self.head_position2 += 1 # move right
+            if self.head_position2 == len(self.tape2):
+                self.tape2.append(self.blank_symbol)
+        elif tape == 3:
+            self.head_position3 += 1 # move right
+            if self.head_position3 == len(self.tape3):
+                self.tape3.append(self.blank_symbol)
 
-    def read_symbols(self):
-        return self.tape[self.head_position]
+    def read_symbols(self, tape):
+        if tape == 1:
+            return self.tape1[self.head_position1]
+        elif tape == 2:
+            return self.tape2[self.head_position2]
+        elif tape == 3:
+            return self.tape3[self.head_position3]
 
-    def write_symbols(self, symbol):
-        self.tape[self.head_position] = symbol
+    def write_symbols(self, tape, symbol):
+        if tape == 1:
+            self.tape1[self.head_position1] = symbol
+        elif tape == 2:
+            self.tape2[self.head_position2] = symbol
+        elif tape == 3:
+            self.tape3[self.head_position3] = symbol
 
     def transition(self):
-        input_symbols = self.read_symbols()
-        transitions = self.current_state.delta(input_symbols)
+        input_symbol1 = self.read_symbols(1)
+        input_symbol2 = self.read_symbols(2)
+        input_symbol3 = self.read_symbols(3)
+        transitions = self.current_state.delta(input_symbol1, input_symbol2, input_symbol3)
 
         if not transitions:
-            print(f"No transition for d({self.current_state.name}, {input_symbols})")
+            print(f"No transition for d({self.current_state.name}, {input_symbol1}, {input_symbol2}, {input_symbol3})")
             return False
 
         transition = transitions[0]
-        next_state, write_symbols, directions = transition['next_state'], transition['write_symbol'], transition['direction']
+        next_state = transition['next_state']
+        write_symbol1 = transition['write_symbol1']
+        write_symbol2 = transition['write_symbol2']
+        write_symbol3 = transition['write_symbol3']
+        direction1 = transition['direction1']
+        direction2 = transition['direction2']
+        direction3 = transition['direction3']
 
-        print(f"d({self.current_state.name}, {input_symbols}) = ({next_state.name}, {write_symbols}, {directions})")
+        print(f"d({self.current_state.name}, {input_symbol1}, {input_symbol2}, {input_symbol3}) = "
+              f"({next_state.name}, {write_symbol1}, {write_symbol2}, {write_symbol3}, {direction1}, {direction2}, {direction3})")
 
         self.current_state = next_state
-        self.write_symbols(write_symbols)
+        self.write_symbols(1, write_symbol1)
+        self.write_symbols(2, write_symbol2)
+        self.write_symbols(3, write_symbol3)
 
-        for i in range(len(self.tapes)):
-            if directions[i] == 'L':
-                self.move_left()
-            elif directions[i] == 'R':
-                self.move_right()
+        if direction1 == 'L':
+            self.move_left(1)
+        elif direction1 == 'R':
+            self.move_right(1)
+
+        if direction2 == 'L':
+            self.move_left(2)
+        elif direction2 == 'R':
+            self.move_right(2)
+
+        if direction3 == 'L':
+            self.move_left(3)
+        elif direction3 == 'R':
+            self.move_right(3)
 
         self.show_tapes()
 
         return True
 
-
     def show_tapes(self):
         print(f"Tapes at {self.current_state.name}:")
-        for i, tape in enumerate(self.tapes):
-            print(f"Tape {i + 1}:", end=" ")
-            for j, symbol in enumerate(tape):
-                if j == self.head_positions[i]:
-                    print(f"|_{symbol}_|", end=" ")
-                else:
-                    print(f"|_{symbol}_|", end=" ")
-            print()
+        # first tape
+        for i, (symbol1, symbol2, symbol3) in enumerate(zip(self.tape1, self.tape2, self.tape3)):
+            print(f"|_{symbol1}_|", end=" ")
+            if i == self.head_position1:
+                print("^", end=" ")
+            else:
+                print(" ", end=" ")
+
+        print()  # Move to a new line for the second tape
+        # second tape
+        for i, (symbol1, symbol2, symbol3) in enumerate(zip(self.tape1, self.tape2, self.tape3)):
+            print(f"|_{symbol2}_|", end=" ")
+            if i == self.head_position2:
+                print("^", end=" ")
+            else:
+                print(" ", end=" ")
+
+        print()  # Move to a new line for the third tape
+        # third tape
+        for i, (symbol1, symbol2, symbol3) in enumerate(zip(self.tape1, self.tape2, self.tape3)):
+            print(f"|_{symbol3}_|", end=" ")
+            if i == self.head_position3:
+                print("^", end=" ")
+            else:
+                print(" ", end=" ")
+
         print("\n")
 
 #option A
@@ -109,24 +180,25 @@ if __name__ == '__main__':
     q2 = State('q2', final=True)
 
     #L = left, R = right, S = stay
-    #define transitions self, input_symbol, write_symbol, next_state, direction
-    q0.set_transition(['0', '0', 'B'], ['0', '0', '0'], q0, ['L', 'L', 'L'])
-    q0.set_transition(['B', 'B', 'B'], ['B', 'B', '0'], q2, ['S', 'S', 'S'])
-    q0.set_transition(['0', '1', 'B'], ['0', '1', '1'], q0, ['L', 'L', 'L'])
-    q0.set_transition(['B', '1', 'B'], ['B', '1', '1'], q0, ['L', 'L', 'L'])
-    q0.set_transition(['1', '0', 'B'], ['1', '0', '1'], q0, ['L', 'L', 'L'])
-    q0.set_transition(['1', 'B', 'B'], ['1', 'B', '1'], q0, ['L', 'L', 'L'])
-    q0.set_transition(['1', '1', 'B'], ['1', '1', '0'], q1, ['L', 'L', 'L'])
+    #define transitions read1, read2, read3, write1, write2, write3, next_state, direction1, direction2, direction3
+    q0.set_transition('0', '0', 'B', '0', '0', '0', q0, 'L', 'L', 'L')
+    q0.set_transition('B', 'B', 'B', 'B', 'B', '0', q2, 'S', 'S', 'S')
+    q0.set_transition('0', '1', 'B', '0', '1', '1', q0, 'L', 'L', 'L')
+    q0.set_transition('B', '1', 'B', 'B', '1', '1', q0, 'L', 'L', 'L')
+    q0.set_transition('1', '0', 'B', '1', '0', '1', q0, 'L', 'L', 'L')
+    q0.set_transition('1', 'B', 'B', '1', 'B', '1', q0, 'L', 'L', 'L')
+    q0.set_transition('1', '1', 'B', '1', '1', '0', q1, 'L', 'L', 'L')
+    
 
-    q1.set_transition(['0', '0', 'B'], ['0', '0', '1'], q0, ['L', 'L', 'L'])
-    q1.set_transition(['B', 'B', 'B'], ['B', 'B', '1'], q2, ['S', 'S', 'S'])
-    q1.set_transition(['0', '1', 'B'], ['0', '1', '0'], q1, ['L', 'L', 'L'])
-    q1.set_transition(['B', '1', 'B'], ['B', '1', '0'], q1, ['L', 'L', 'L'])
-    q1.set_transition(['1', '0', 'B'], ['1', '0', '0'], q1, ['L', 'L', 'L'])
-    q1.set_transition(['1', 'B', 'B'], ['1', 'B', '0'], q1, ['L', 'L', 'L'])
-    q1.set_transition(['1', '1', 'B'], ['1', '1', '1'], q1, ['L', 'L', 'L'])
+    q1.set_transition('0', '0', 'B', '0', '0', '1', q0, 'L', 'L', 'L')
+    q1.set_transition('B', 'B', 'B', 'B', 'B', '1', q2, 'S', 'S', 'S')
+    q1.set_transition('0', '1', 'B', '0', '1', '0', q1, 'L', 'L', 'L')
+    q1.set_transition('B', '1', 'B', 'B', '1', '0', q1, 'L', 'L', 'L')
+    q1.set_transition('1', '0', 'B', '1', '0', '0', q1, 'L', 'L', 'L')
+    q1.set_transition('1', 'B', 'B', '1', 'B', '0', q1, 'L', 'L', 'L')
+    q1.set_transition('1', '1', 'B', '1', '1', '1', q1, 'L', 'L', 'L')
 
-    mtm = MultiTapeTuringMachine(states=[q0, q1, q2], symbols=['0', '1'], input_string=["101", "110"])
+    mtm = MultiTapeTuringMachine(states=[q0, q1, q2], symbols=['0', '1'], input_string1="0101", input_string2="0100", blank_symbol='B')
 
     while mtm.transition():
         pass
