e5eb36c8aea4b6ecebd046f85fcc9b9158cb3a38
John Luis Magtoto <jfmagtoto@up.edu.ph>
2024-01-26 11:59:32 +0800

    Fix the class state to be compatible to 3 tapes

--- ProjectMultiTapeTM.py
+++ ProjectMultiTapeTM.py
@@ -4,19 +4,28 @@ class State:
         self.final = final
         self.next = {}
 
-    #get the symbol from the tape list and the next state
-    def delta(self, symbol):
-        return self.next.get(symbol, []) + self.next.get('', [])
+    #get the symbols from the tapes and the next state
+    def delta(self, symbol1, symbol2, symbol3):
+        return self.next.get((symbol1, symbol2, symbol3), []) + self.next.get(('', symbol2, symbol3), []) + \
+               self.next.get((symbol1, '', symbol3), []) + self.next.get((symbol1, symbol2, ''), []) + \
+               self.next.get(('', '', symbol3), []) + self.next.get(('', symbol2, ''), []) + \
+               self.next.get((symbol1, '', ''), []) + self.next.get(('','', ''), [])
     
-    #transition function that returns the next state, write symbol, and direction based on the input symbol.
-    def set_transition(self, input_symbol, write_symbol, next_state, direction):
-        if input_symbol not in self.next:
-            self.next[input_symbol] = []
+    #transition function that returns the next state, write symbols, and directions based on the input symbols.
+    def set_transition(self, input_symbol1, input_symbol2, input_symbol3,
+                        write_symbol1, write_symbol2, write_symbol3, 
+                        next_state, direction1, direction2, direction3):
+        if (input_symbol1, input_symbol2, input_symbol3) not in self.next:
+            self.next[(input_symbol1, input_symbol2, input_symbol3)] = []
         #function that sets transitions
-        self.next[input_symbol].append({
+        self.next[(input_symbol1, input_symbol2, input_symbol3)].append({
             'next_state': next_state,
-            'write_symbol': write_symbol,
-            'direction': direction
+            'write_symbol1': write_symbol1,
+            'write_symbol2': write_symbol2,
+            'write_symbol3': write_symbol3,
+            'direction1': direction1,
+            'direction2': direction2,
+            'direction3': direction3
         })
 
     #set the state as final
@@ -25,12 +34,12 @@ class State:
 
 
 class MultiTapeTuringMachine:
-    def __init__(self, states, symbols, input_strings, blank_symbol='B'):
+    def __init__(self, states, symbols, input_string, blank_symbol='B'):
         self.q = states
-        self.sigma = symbols  # alphabet
+        self.sigma = symbols #alphabet
         self.blank_symbol = blank_symbol
-        self.tapes = [[blank_symbol] + list(input_string) for input_string in input_strings]
-        self.head_positions = [len(input_string) for input_string in input_strings]  # Tape heads point to the rightmost input symbols
+        self.tape = [blank_symbol] + list(input_string) + [blank_symbol] 
+        self.head_position = 1  # Start reading from the first character of the input
         self.current_state = states[0]
 
     def move_left(self):
@@ -46,42 +55,38 @@ class MultiTapeTuringMachine:
             if self.head_positions[i] == len(self.tapes[i]):
                 self.tapes[i].append(self.blank_symbol)
 
-    def read_symbol(self):
-        return [tape[head_position] for tape, head_position in zip(self.tapes, self.head_positions)]
+    def read_symbols(self):
+        return self.tape[self.head_position]
 
-    def write_symbol(self, symbols):
-        for i in range(len(self.tapes)):
-            self.tapes[i][self.head_positions[i]] = symbols[i]
+    def write_symbols(self, symbol):
+        self.tape[self.head_position] = symbol
 
     def transition(self):
         input_symbols = self.read_symbols()
-        transitions = self.current_state.delta(input_symbols)  # Responsible for returning the set of transitions based on the input symbols.
+        transitions = self.current_state.delta(input_symbols)
 
-        # If no next transition from input symbols
         if not transitions:
             print(f"No transition for d({self.current_state.name}, {input_symbols})")
             return False
 
         transition = transitions[0]
-        next_state, write_symbols, directions = transition['next_state'], transition['write_symbols'], transition['directions']
+        next_state, write_symbols, directions = transition['next_state'], transition['write_symbol'], transition['direction']
 
-        # Display the transition function
         print(f"d({self.current_state.name}, {input_symbols}) = ({next_state.name}, {write_symbols}, {directions})")
 
         self.current_state = next_state
         self.write_symbols(write_symbols)
 
-        # Manipulate the directions of the tapes (either left or right)
         for i in range(len(self.tapes)):
             if directions[i] == 'L':
                 self.move_left()
             elif directions[i] == 'R':
                 self.move_right()
 
-        # Print the tapes
         self.show_tapes()
 
         return True
+
 
     def show_tapes(self):
         print(f"Tapes at {self.current_state.name}:")
@@ -121,7 +126,7 @@ if __name__ == '__main__':
     q1.set_transition(['1', 'B', 'B'], ['1', 'B', '0'], q1, ['L', 'L', 'L'])
     q1.set_transition(['1', '1', 'B'], ['1', '1', '1'], q1, ['L', 'L', 'L'])
 
-    mtm = MultiTapeTuringMachine(states=[q0, q1, q2], symbols=['0', '1'], input_strings=["101", "110"])
+    mtm = MultiTapeTuringMachine(states=[q0, q1, q2], symbols=['0', '1'], input_string=["101", "110"])
 
     while mtm.transition():
         pass
